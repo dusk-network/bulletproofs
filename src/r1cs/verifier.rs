3d5d3291b984569de31de50463b497d7272f475b
zer0 <matteo@dusk.network>
2019-09-20 16:19:33 +0200

    Add the `rng` as `verify` parameter
    Instead of instantiate the `rng` inside the function, `verify` takes
    the generator as argument, so the caller can decided which one to use.

--- src/r1cs/verifier.rs
+++ src/r1cs/verifier.rs
@@ -5,6 +5,7 @@ use curve25519_dalek::ristretto::{CompressedRistretto, RistrettoPoint};
 use curve25519_dalek::scalar::Scalar;
 use curve25519_dalek::traits::VartimeMultiscalarMul;
 use merlin::Transcript;
+use rand;
 
 use super::{
     ConstraintSystem, LinearCombination, R1CSProof, RandomizableConstraintSystem,
@@ -318,12 +319,14 @@ impl<'t> Verifier<'t> {
     /// [`BulletproofGens`] should have `gens_capacity` greater than
     /// the number of multiplication constraints that will eventually
     /// be added into the constraint system.
-    pub fn verify(
+    pub fn verify<R>(
         mut self,
         proof: &R1CSProof,
         pc_gens: &PedersenGens,
         bp_gens: &BulletproofGens,
-    ) -> Result<(), R1CSError> {
+        thread_rng: &mut R) -> Result<(), R1CSError>
+        where
+            R: rand::Rng + rand::CryptoRng    {
         // Commit a length _suffix_ for the number of high-level variables.
         // We cannot do this in advance because user can commit variables one-by-one,
         // but this suffix provides safe disambiguation because each variable
@@ -436,8 +439,7 @@ impl<'t> Verifier<'t> {
         // Create a `TranscriptRng` from the transcript. The verifier
         // has no witness data to commit, so this just mixes external
         // randomness into the existing transcript.
-        use rand::thread_rng;
-        let mut rng = self.transcript.build_rng().finalize(&mut thread_rng());
+        let mut rng = self.transcript.build_rng().finalize(thread_rng);
         let r = Scalar::random(&mut rng);
 
         let xx = x * x;
